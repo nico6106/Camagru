48563857aedb8c8368af3eeb51ea6012fc7c631f
Nicolas LESAGE <nlesage@made-f0Dr11s12.clusters.42paris.fr>
2023-09-23 18:52:00 +0200

    navbar with outside click handler

--- frontend/mount/src/components/NavBar.tsx
+++ frontend/mount/src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import OutsideClickHandler from "react-outside-click-handler";
 
 function MobileMenuBoutton({
   showMenu,
@@ -15,15 +16,13 @@ function MobileMenuBoutton({
         className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
         aria-controls="mobile-menu"
         aria-expanded="false"
-        onClick={() => setShowMenu(!showMenu)}
+        onClick={() => {
+          setShowMenu(!showMenu);
+        }}
       >
         <span className="absolute -inset-0.5"></span>
         <span className="sr-only">Open main menu</span>
-        {/* <!--
-		Icon when menu is closed.
 
-		Menu open: "hidden", Menu closed: "block"
-	  --> */}
         <svg
           className="block h-6 w-6"
           fill="none"
@@ -36,25 +35,6 @@ function MobileMenuBoutton({
             strokeLinecap="round"
             strokeLinejoin="round"
             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-        {/* <!--
-		Icon when menu is open.
-
-		Menu open: "block", Menu closed: "hidden"
-	  --> */}
-        <svg
-          className="hidden h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 18L18 6M6 6l12 12"
           />
         </svg>
       </button>
@@ -177,51 +157,57 @@ function DropdownMenuLinks() {
   );
 }
 function DropdownMenu() {
-  const [showMenu, setShowMenu] = useState<boolean>(false);
+  const [showDropMenu, setShowDropMenu] = useState<boolean>(false);
   // console.log('menu')
   return (
     <div className="relative ml-3">
-      <div>
-        <button
-          type="button"
-          className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-          id="user-menu-button"
-          aria-expanded="false"
-          aria-haspopup="true"
-          onClick={() => setShowMenu(!showMenu)}
-        >
-          <span className="absolute -inset-1.5"></span>
-          <span className="sr-only">Open user menu</span>
-          <img
-            className="h-8 w-8 rounded-full"
-            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-            alt=""
-          />
-        </button>
-      </div>
+      <OutsideClickHandler
+        onOutsideClick={() => {
+          setShowDropMenu(false);
+        }}
+      >
+        <div>
+          <button
+            type="button"
+            className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            id="user-menu-button"
+            aria-expanded="false"
+            aria-haspopup="true"
+            onClick={() => setShowDropMenu(!showDropMenu)}
+          >
+            <span className="absolute -inset-1.5"></span>
+            <span className="sr-only">Open user menu</span>
+            <img
+              className="h-8 w-8 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+          </button>
+        </div>
 
-      {/* <!--
-	Dropdown menu, show/hide based on menu state.
-
-	Entering: "transition ease-out duration-100"
-	  From: "transform opacity-0 scale-95"
-	  To: "transform opacity-100 scale-100"
-	Leaving: "transition ease-in duration-75"
-	  From: "transform opacity-100 scale-100"
-	  To: "transform opacity-0 scale-95"
-  --> */}
-      {showMenu && <DropdownMenuLinks />}
+        {showDropMenu && <DropdownMenuLinks />}
+      </OutsideClickHandler>
     </div>
   );
 }
 
 function NavBar() {
   const [showMenu, setShowMenu] = useState<boolean>(false);
+  let outside: boolean = false;
   return (
     <nav className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <MobileMenuBoutton showMenu={showMenu} setShowMenu={setShowMenu} />
+          <OutsideClickHandler
+            onOutsideClick={() => {
+			  outside = true;
+            }}
+          >
+            <MobileMenuBoutton
+              showMenu={showMenu}
+              setShowMenu={setShowMenu}
+            />
+          </OutsideClickHandler>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <LogoNavBar />
             <LinkNavBar />
@@ -232,7 +218,15 @@ function NavBar() {
           </div>
         </div>
       </div>
-      <MobileMenu showMenu={showMenu} />
+      <OutsideClickHandler
+        onOutsideClick={() => {
+        //   console.log("outside small menu " + showMenu);
+          if (outside) setShowMenu(false);
+		  outside = false;
+        }}
+      >
+        <MobileMenu showMenu={showMenu} />
+      </OutsideClickHandler>
     </nav>
   );
 }
