52e9f2ec8522e29393416e12fd6c17d458b3aceb
Nicolas LESAGE <nlesage@made-f0Dr11s12.clusters.42paris.fr>
2023-09-24 11:13:25 +0200

    adding pgadmin service to the dockerfile

--- backend/backend.sh
+++ backend/backend.sh
@@ -7,26 +7,29 @@ export DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@dev-db:$
 npm install
 
 
-sleep 5
-pg_isready -U ${POSTGRES_USER} -h localhost -p 5432
+# sleep 5
+# pg_isready -U ${POSTGRES_USER} -h localhost -p 5432
 
-psql -U ${POSTGRES_USER} -d test -c "SELECT 1;" 2>/dev/null
-EXIT_CODE=$?
-echo "bdd exitcode=" $EXIT_CODE
-if [ $EXIT_CODE -eq 0 ]; then
-	echo "La base de données 'test' existe déjà."
-else
-	echo "Creating DB"
+# psql -U ${POSTGRES_USER} -d test -c "SELECT 1;" 2>/dev/null
+# EXIT_CODE=$?
+# echo "bdd exitcode=" $EXIT_CODE
+# if [ $EXIT_CODE -eq 0 ]; then
+# 	echo "La base de données 'test' existe déjà."
+# else
+# 	echo "Creating DB"
 
-	psql -U ${POSTGRES_USER} -c "CREATE DATABASE test;"
+# 	psql -U ${POSTGRES_USER} -c "CREATE DATABASE test;"
 
-	if [ $? -eq 0 ]; then
-    	echo "La base de données 'test' a été créée avec succès."
-	else
-		echo "Erreur lors de la création de la base de données 'test'."
-	fi
+# 	if [ $? -eq 0 ]; then
+#     	echo "La base de données 'test' a été créée avec succès."
+# 	else
+# 		echo "Erreur lors de la création de la base de données 'test'."
+# 	fi
 
-fi
+# fi
+
+
+
 
 # npx prisma generate
 
