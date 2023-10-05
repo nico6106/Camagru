07768c0dbdd2307148fdbd83b97abaa75b4b96b2
Nicolas LESAGE <nlesage@paul-f4Ar7s1.clusters.42paris.fr>
2023-10-05 11:19:22 +0200

    working on photo uplaod

--- backend/mount/custom.d.ts
+++ backend/mount/custom.d.ts
@@ -2,12 +2,6 @@ import { Request } from 'express';
 import { Multer } from 'multer';
 
 declare module 'express' {
-    interface Request {
-        newName: string;
-    }
-}
-
-declare module 'express' {
 	interface Request {
 	  file: Multer.File;
 	}
--- backend/mount/src/routes/users/users.controllers.ts
+++ backend/mount/src/routes/users/users.controllers.ts
@@ -1,5 +1,8 @@
 import { Database } from "../../database/db"
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
+import { Multer } from 'multer';
+import { extname } from 'path';
+
 import { checkConnected } from "../auth/middlewares/check-connection.middleware";
 import { getMe, updateSettings, uploadImg } from "./users.service";
 import { validateSignUpBody } from "../auth/middlewares/signup.middleware";
@@ -31,34 +34,64 @@ router.post('/photo', checkConnected, (req: Request, res: Response) => {
 	return getMe(db, req, res);
 })
 
-const multer = require('multer');
-// Create multer object
-const storage = multer.diskStorage({
-	destination: 'images',
-	filename: (req: Request, file: any, cb: any) => {
-	  cb(null, Date.now() + '-' + file.originalname);
-	},
-  });
 
-  
+// Create multer object
+// const storage = multer.diskStorage({
+// 	destination: 'images',
+// 	filename: (req: Request, file: any, cb: any) => {
+// 	  cb(null, Date.now() + '-' + file.originalname);
+// 	},
+//   });
+
+
+const multer = require('multer');
+
 const imageUpload = multer({
-	dest: 'images',
+    storage: multer.diskStorage(
+        {
+            destination: function (req: any, file: any, cb: any) {
+                cb(null, 'images/');
+            },
+            filename: function (req: any, file: any, cb: any) {
+				const name1 = file.originalname.split('.')[0];
+				const name = name1.split(' ').join('_');
+				const fileExtName = extname(file.originalname);
+				console.log(fileExtName)
+
+                cb(
+                    null,
+                    new Date().valueOf() + 
+                    '_' +
+                    name + fileExtName
+                );
+            }
+        }
+    ), 
 });
 
+
 router.post('/image', imageUpload.single('image'), (req: Request, res: Response) => { 
-	console.log(req.file);
+
+	// const request: Express.Request = req;
+    // const files: Express.Multer.File[] = request.files;
+
+	const file: Express.Multer.File | undefined = req.file;
+
+
+	console.log(file);
 	return res.status(200).json({ message: 'success' });
 	// return uploadImg(db, req, res); 
 });
 
 
+const path = require('path');
 
 // Image Get Routes
 router.get('/image/:filename', (req: Request, res: Response) => {
-	// const { filename } = req.params;
-    // const dirname = path.resolve();
-    // const fullfilepath = path.join(dirname, 'images/' + filename);
-    // return res.sendFile(fullfilepath);
+	const { filename } = req.params;
+    const dirname = path.resolve();
+    const fullfilepath = path.join(dirname, 'images/' + filename);
+    return res.sendFile(fullfilepath);
 
 
     return getMe(db, req, res);
