8c71c5713003ec234dbe0edcc896e60402ad32d4
Nicolas LESAGE <nlesage@made-f0Dr11s12.clusters.42paris.fr>
2023-10-09 10:25:46 +0200

    adding socket-io on frontend side

---
+++ frontend/mount/src/components/auth/TrackingOnline.tsx
@@ -0,0 +1,60 @@
+import { useContext, useEffect, useState } from 'react';
+import { WebsocketContext } from '../../context/WebsocketContext';
+import { useUserContext } from '../../context/UserContext';
+
+type TypeTracking = {
+    id: number;
+    idConnection: string;
+};
+
+function TrackingOnline() {
+    const socket = useContext(WebsocketContext);
+    const { user } = useUserContext();
+    const [userObj, setUserObj] = useState<TypeTracking>();
+
+    useEffect(() => {
+        setUserObj({
+            id: user ? user.id : -1,
+            idConnection: socket.id,
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        socket.on('connect', () => {
+            setUserObj({
+                id: user ? user.id : -1,
+                idConnection: socket.id,
+            });
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [socket]);
+
+    useEffect(() => {
+        setUserObj({
+            id: user ? user.id : -1,
+            idConnection: socket.id,
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [user]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const userObja: TypeTracking = {
+                id: user ? user.id : -1,
+                idConnection: socket.id,
+            }
+            socket.emit('ping', userObja);
+        }, 500);
+
+        return () => {
+            socket.off('ping');
+            clearInterval(interval);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [userObj]);
+
+    return <></>;
+}
+
+export default TrackingOnline;
---
+++ frontend/mount/src/context/WebsocketContext.tsx
@@ -0,0 +1,8 @@
+import { createContext } from 'react';
+import { io, Socket } from 'socket.io-client';
+
+export const socket = io(
+    `http://${process.env.REACT_APP_SERVER_ADDRESS}:3333/`,
+);
+export const WebsocketContext = createContext<Socket>(socket);
+export const WebsocketProvider = WebsocketContext.Provider;
